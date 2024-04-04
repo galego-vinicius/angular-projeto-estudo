51a0fea32752023f53b139e294c524b793c726b4
vinicius.felix <vfelix1998@gmail.com>
2024-04-04 13:31:14 -0300

    add: Routs and Nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'home', component:HomeComponent },
+    { path:'contact', component:ContactComponent },
+    { path:'about', component:AboutComponent },
+    { path:'gallery', component:GalleryComponent },
+    { path:'**', component:NotFoundComponent },
+];
